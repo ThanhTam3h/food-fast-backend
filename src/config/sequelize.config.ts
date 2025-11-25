c5e42c29e1e8f4675869e731024170d098c8fe2e
ThanhTam3h <dao.thi.thanh.tam.0807@gmail.com>
2025-11-26 02:12:13 +0700

    fixx

--- src/config/sequelize.config.ts
+++ src/config/sequelize.config.ts
@@ -1,42 +1,45 @@
 import { ConfigService } from '@nestjs/config';
 import { SequelizeModuleOptions } from '@nestjs/sequelize';
 import { Dialect } from 'sequelize';
+import { Address } from 'src/models/address.model';
+import { Cart } from 'src/models/cart.model';
+import { CartItem } from 'src/models/cart_item.model';
+import { CartItemTopping } from 'src/models/cart_item_topping.model';
+import { Category } from 'src/models/category.model';
+import { Merchant } from 'src/models/merchant.model';
+import { Order } from 'src/models/order.model';
+import { OrderItem } from 'src/models/order_item.model';
+import { OrderItemTopping } from 'src/models/order_item_topping.model';
+import { Permission } from 'src/models/permission.model';
+import { Product } from 'src/models/product.model';
+import { Role } from 'src/models/role.model';
+import { RolePermission } from 'src/models/role_permission.model';
+import { Topping } from 'src/models/topping.model';
+import { User } from 'src/models/user.model';
+import { UserRole } from 'src/models/user_role.model';
+
 import {
-  Address,
-  Cart,
-  CartItem,
-  CartItemTopping,
-  Category,
-  Merchant,
-  MerchantImage,
-  Order,
-  OrderItem,
-  OrderItemTopping,
-  Permission,
-  Product,
-  Role,
-  RolePermission,
-  Topping,
-  User,
-  UserRole,
-  ProductToppingGroup,
-  ToppingGroup,
   Drone,
   DroneHub,
+  MerchantImage,
+  ProductToppingGroup,
+  ToppingGroup,
 } from 'src/models';
 
 export const sequelizeConfig = (
   configService: ConfigService,
 ): SequelizeModuleOptions => ({
-  // Quan trọng: dùng URL
-  url: configService.get<string>('postgresql://neondb_owner:yourpassword@ep-snowy-snow-a116r8uj-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'),
-
-  dialect: 'postgres',
+  database: configService.get<string>('DB_NAME'),
+  username: configService.get<string>('DB_USERNAME'),
+  password: configService.get<string>('DB_PASSWORD'),
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT')
+    ? Number(configService.get<number>('DB_PORT'))
+    : 5432,
+  dialect: configService.get<Dialect>('DB_DIALECT') ?? 'postgres',
   synchronize: false,
-  autoLoadModels: true,
-  logging: false,
-
   models: [
+    // Cha
     Topping,
     Address,
     User,
@@ -59,6 +62,8 @@ export const sequelizeConfig = (
     DroneHub,
     Drone,
   ],
+  autoLoadModels: true,
+  logging: false,
 
   dialectOptions: {
     ssl: {
