ee6171d04b9c16034c3951d5e2882aa3ad2ef963
ThanhTam3h <dao.thi.thanh.tam.0807@gmail.com>
2025-11-25 23:03:19 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -11,13 +11,14 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
    app.enableCors({
-    origin: '*', // URL mặc định Vite
+    origin: 'https://food-fast-frontend.vercel.app', // URL mặc định Vite
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
   app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://food-fast-frontend.vercel.app');
+    res.header("Access-Control-Allow-Credentials", "true");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
