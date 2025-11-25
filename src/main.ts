27983e2bd6f4fc2b6279984d62af753409590351
ThanhTam3h <dao.thi.thanh.tam.0807@gmail.com>
2025-11-25 22:56:08 +0700

    fix

--- src/main.ts
+++ src/main.ts
@@ -11,9 +11,20 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
    app.enableCors({
-    origin: true, // URL mặc định Vite
+    origin: '*', // URL mặc định Vite
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
+  });
+
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+      return res.sendStatus(200);
+    }
+    next();
   });
 
   const logger = new Logger('bootstrap');
